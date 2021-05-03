4718117853702826c9614a15b3ce90a8b3ecab68
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-05-03 10:27:49 +0530

    Added the SEO Component

--- src/lib/shared/components/index.ts
+++ src/lib/shared/components/index.ts
@@ -2,3 +2,4 @@ export { ApplicationTitles } from './application-titles';
 export { Counter } from './counter';
 export { HeadTags } from './head-tags';
 export { Title } from './title';
+export { SEO } from './seo';
--- src/lib/shared/ui/components/index.ts
+++ src/lib/shared/ui/components/index.ts
@@ -1,6 +1,3 @@
 export { Footer } from './footer';
 export { Header } from './header';
-export { SEO } from './seo';
-
-// UI Components
 export { Card } from './card';
