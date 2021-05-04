93eb0240874df28ea29b6d4635bdf6b61e6c09bf
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-05-05 01:44:38 +0530

    Updated the sveltekit configuration to support the latest build process

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,48 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* jshint esversion: 9 */
 
 // command env properties
-const adapt = process.env.ADAPTER;
+const hasAdapter = process.env.ADAPTER;
+const adapt = hasAdapter ? hasAdapter : 'node';
 const isSSR = process.env.SSR ? true : false;
 const isAMP = process.env.AMP ? true : false;
 
 // Imports
-const sveltePreprocess = require('svelte-preprocess');
-const pkg = require('./package.json');
-const { resolve } = require('path');
-const adapter = require(adapt || '@sveltejs/adapter-node');
+import sveltePreprocess from 'svelte-preprocess';
+import { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import staticAdapter from '@sveltejs/adapter-static';
+import nodeAdapter from '@sveltejs/adapter-node';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
+import vercelAdapter from '@sveltejs/adapter-vercel';
+import { createRequire } from 'module';
+
+const customRequire = createRequire(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkg = customRequire('./package.json');
+
 const options = JSON.stringify(process.env.OPTIONS || '{}');
 
+function getAdapters(adapt) {
+	switch (adapt) {
+		case 'node':
+			return nodeAdapter;
+		case 'static':
+			return staticAdapter;
+		case 'netlify':
+			return netlifyAdapter;
+		case 'vercel':
+			return vercelAdapter;
+		default:
+			return nodeAdapter;
+	}
+}
+
+const adapter = getAdapters(adapt);
+
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
@@ -28,7 +57,6 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: adapter(options),
 		ssr: isSSR,
 		amp: isAMP,
 
@@ -59,3 +87,9 @@ module.exports = {
 		}),
 	},
 };
+
+if (hasAdapter) {
+	config.kit.adapter = adapter(options);
+}
+
+export default config;
