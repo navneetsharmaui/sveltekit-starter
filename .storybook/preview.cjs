c88bc96cc6d150ce54c60c66fd0687af3450e824
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-05-06 00:38:42 +0530

    Added the global styles support

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,6 +1,9 @@
 /* jshint esversion: 9 */
 
 const { withInfo } = require('@storybook/addon-info');
+// import '!style-loader!css-loader!sass-loader!./scss-loader.scss';
+
+import '../static/global.css';
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
