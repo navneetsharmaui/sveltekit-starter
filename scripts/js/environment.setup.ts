0402f6c5327b2e2e452f5299ca31630a99a34a88
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-05-20 00:00:11 +0530

    Updated the environment set up

--- scripts/js/environment.setup.ts
+++ scripts/js/environment.setup.ts
@@ -1,18 +1,27 @@
 import fs from 'fs';
 
-const pathToEnvironmentPorpertiesFolder = './src/environments';
+const pathToWorkspaceJSON = './workspace.json';
+
+// const pathToEnvironmentPorpertiesFolder = './src/environments';
 
 const mode = process.env.MODE;
-
-let pages = '';
+const project = process.env.PROJECT;
+const architectType = process.env.ARCHITECT_TYPE;
+const configuration = process.env.configurations;
 const fileToRead = mode ? mode : 'local';
 
-fs.readdirSync(pathToEnvironmentPorpertiesFolder).forEach((file) => {
-	if (file.includes(fileToRead)) {
-		pages = file;
-	}
+const writeToFile = (filePath: string, data): void => fs.writeFileSync(filePath, data);
+
+const workspace = JSON.parse(
+	fs.readFileSync(pathToWorkspaceJSON, {
+		encoding: 'utf8',
+	}),
+);
+const root = workspace['projects'][project]['root'];
+const fileReplacements =
+	workspace['projects'][project]['architect'][architectType]['configurations'][fileToRead]['fileReplacements'];
+
+fileReplacements.forEach((value) => {
+	const content = fs.readFileSync(`${root}${value.with}`);
+	writeToFile(`${root}${value.replace}`, content);
 });
-
-const environmentFileContent = fs.readFileSync(`${pathToEnvironmentPorpertiesFolder}/${pages}`);
-
-fs.writeFileSync(`${pathToEnvironmentPorpertiesFolder}/environment.ts`, environmentFileContent);
