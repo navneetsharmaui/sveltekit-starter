f40f5fab9fae141381e63c5b8248d253e906d706
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-05-20 01:03:10 +0530

    Updated the environments setup configuration

--- scripts/js/environment.setup.ts
+++ scripts/js/environment.setup.ts
@@ -1,27 +1,40 @@
+/* eslint-disable no-console */
 import fs from 'fs';
 
 const pathToWorkspaceJSON = './workspace.json';
 
 // const pathToEnvironmentPorpertiesFolder = './src/environments';
 
-const mode = process.env.MODE;
-const project = process.env.PROJECT;
-const architectType = process.env.ARCHITECT_TYPE;
-const configuration = process.env.configurations;
-const fileToRead = mode ? mode : 'local';
+const getArguments = (argv: string[], flag: string): string =>
+	argv
+		.filter((value) => value.includes(flag))
+		.map((value) => value.split('=')[1])
+		.reduce((value) => value);
 
-const writeToFile = (filePath: string, data): void => fs.writeFileSync(filePath, data);
+const processArguments = process.argv;
 
-const workspace = JSON.parse(
-	fs.readFileSync(pathToWorkspaceJSON, {
-		encoding: 'utf8',
-	}),
-);
-const root = workspace['projects'][project]['root'];
-const fileReplacements =
-	workspace['projects'][project]['architect'][architectType]['configurations'][fileToRead]['fileReplacements'];
+try {
+	const mode = getArguments(processArguments, '--mode');
+	const project = getArguments(processArguments, '--project');
+	const architectType = getArguments(processArguments, '--arhitect-type');
+	// const configuration = getArguments(processArguments, '--configurations');
+	const fileToRead = mode ? mode : 'local';
 
-fileReplacements.forEach((value) => {
-	const content = fs.readFileSync(`${root}${value.with}`);
-	writeToFile(`${root}${value.replace}`, content);
-});
+	const writeToFile = (filePath: string, data): void => fs.writeFileSync(filePath, data);
+
+	const workspace = JSON.parse(
+		fs.readFileSync(pathToWorkspaceJSON, {
+			encoding: 'utf8',
+		}),
+	);
+	const root = workspace['projects'][project]['root'];
+	const fileReplacements =
+		workspace['projects'][project]['architect'][architectType]['configurations'][fileToRead]['fileReplacements'];
+
+	fileReplacements.forEach((value) => {
+		const content = fs.readFileSync(`${root}${value.with}`);
+		writeToFile(`${root}${value.replace}`, content);
+	});
+} catch (error) {
+	console.error(error);
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 import {
 	EnvironmentName,
 	EnvironmentType,
-	SVELTEKIT_DATA_ENPOINTS,
+	SVELTEKIT_DATA_ENPOINTS_DEV,
 	SVELTEKIT_STARTER_ENPOINT_CONFIG,
-	SVELTEKIT_SEARCH_ENPOINTS,
+	SVELTEKIT_SEARCH_ENPOINTS_DEV,
 } from '$lib/models';
 import type { ISveltekitStarterEnvironmentConfig } from '$models/interfaces/isveltekit-strater-environment.interface';
 
 export const environment: ISveltekitStarterEnvironmentConfig<SVELTEKIT_STARTER_ENPOINT_CONFIG> = {
-	name: EnvironmentName.LOCAL,
-	environmentType: EnvironmentType.LOCAL,
-	production: false,
-	isDebugMode: true,
+	name: EnvironmentName.DEVELOPMENT,
+	environmentType: EnvironmentType.DEV,
+	production: true,
+	isDebugMode: false,
 	apiUrls: {
 		CHUCK_NORRIS: import.meta.env.VITE_CHUCK_NORRIS_API_URL,
 		IN_MEMORY: '',
@@ -22,8 +22,8 @@ export const environment: ISveltekitStarterEnvironmentConfig<SVELTEKIT_STARTER_E
 			? import.meta.env.VITE_CHUCK_NORRIS_API_LANG
 			: 'en-US',
 		endPoints: {
-			SEARCH: SVELTEKIT_SEARCH_ENPOINTS.SEARCH,
-			SERVICE: SVELTEKIT_DATA_ENPOINTS.SERVICE,
+			SEARCH: SVELTEKIT_SEARCH_ENPOINTS_DEV.SEARCH,
+			SERVICE: SVELTEKIT_DATA_ENPOINTS_DEV.SERVICE,
 		},
 	},
 };
