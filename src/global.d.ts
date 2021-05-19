bd1ccd4ff7013e419f9789c3d6816d81c6593aa9
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-05-20 03:18:18 +0530

    Added the global declarations to import images into the components

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,7 +4,67 @@
 
 /// <reference types="@storybook/addon-svelte-csf" />
 
+// Modules
 declare module '@storybook/addon-svelte-csf';
+
+/**
+ * Wiht these declarations images can be imported in the components.
+ *
+ * @example
+ * ```svelte
+ * 	<script lang=ts>
+ * 		import logo from 'static/images/logo.svg';
+ * 	</script>
+ *
+ * 	<img src="{logo}" />
+ * ```
+ */
+declare module '*.gif' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.jpg' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.jpeg' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.png' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.JPG' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.JPEG' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.PNG' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.svg' {
+	const value: string;
+	export = value;
+}
+
+declare module '*.wbep' {
+	const value: string;
+	export = value;
+}
+
+// Start: Interfaces
 
 interface ImportMetaEnv {
 	VITE_BASE_URL: string;
