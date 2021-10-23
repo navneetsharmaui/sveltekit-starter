9f4d69120a788151a0309c6447ac2af9955ae07d
Christoph <46030718+chranditho@users.noreply.github.com>
2021-10-23 12:06:20 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-e/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* jshint esversion: 9 */
 
 // command env properties
