d2f2d587d61847d46d15b7686982da32a87c26fa
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-11-28 16:40:20 +0530

    Removed the redundant title components

--- src/lib/shared/components/application-titles/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ApplicationTitles } from './ApplicationTitles.svelte';
--- src/lib/shared/components/title/Title.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let title = 'Svelte';
-</script>
-
-<svelte:head>
-	<title>{title}</title>
-</svelte:head>
