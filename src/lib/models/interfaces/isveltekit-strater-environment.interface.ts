4bc6213154299a89290ebc95abaaf80e9575e8a1
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-06 17:00:02 +0530

    Removed the session property from the environment config interface

--- src/lib/models/interfaces/isveltekit-strater-environment.interface.ts
+++ src/lib/models/interfaces/isveltekit-strater-environment.interface.ts
@@ -2,14 +2,12 @@ import type { SVELTEKIT_STARTER_API_CONFIG, SVELTEKIT_DATA_ENPOINT_CONFIG } from
 import type { IEnvironmentConfig } from './ienvironment.interface';
 import type { ISveltekitDBConfig } from './isveltekit-db-config.interface';
 import type { ITwitterConfig } from './itwitter-config.interface';
-import type { ISessionConfig } from './isession-config.interface';
 import type { IGitHubConfig } from './igithub-config.interface';
 
 export interface ISveltekitStarterEnvironmentConfig<T = SVELTEKIT_DATA_ENPOINT_CONFIG>
 	extends IEnvironmentConfig {
 	apiUrls: SVELTEKIT_STARTER_API_CONFIG;
 	chuckNorriesAPIConfig: Partial<ISveltekitDBConfig<T>>;
-	sessionConfig: Partial<ISessionConfig>;
 	twitterConfig: Partial<ITwitterConfig>;
 	gitHubConfig: Partial<IGitHubConfig>;
 }
