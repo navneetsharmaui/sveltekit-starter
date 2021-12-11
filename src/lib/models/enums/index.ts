9d677ea068bcb5e6676b16f4a8c63086f518809a
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-11 18:30:51 +0530

    removed the index.ts for export of enums

--- src/lib/models/enums/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './apis.enum';
-export * from './environment-name.enum';
-export * from './environment-type.enum';
-export * from './sveltekit-endpoint.enum';
-export * from './sveltekit-endpoint.prod.enum';
-export * from './sveltekit-endpoint.dev.enum';
-export * from './sveltekit-endpoint.qa.enum';
