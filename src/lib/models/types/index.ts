6d298b95c222db842ca239539afcd22f69f58476
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-11 18:31:08 +0530

    removed the index.ts for export of types

--- src/lib/models/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './api-config.type';
-export * from './apis.type';
-export * from './sveltekit-apis.type';
-export * from './sveltekit-endpoits.type';
-export * from './sveltekit-starter-api-config.type';
