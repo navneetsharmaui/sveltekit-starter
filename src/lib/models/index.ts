21bbb177726b1d71d390c42bd53ed7822b47d645
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-12 18:31:33 +0530

    Updated the types imports in sveltekit stater interface

--- src/lib/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './interfaces';
-export * from './classes';
-export * from './enums';
-export * from './types';
--- src/lib/models/interfaces/isveltekit-strater-environment.interface.ts
+++ src/lib/models/interfaces/isveltekit-strater-environment.interface.ts
@@ -1,4 +1,5 @@
-import type { SVELTEKIT_STARTER_API_CONFIG, SVELTEKIT_DATA_ENPOINT_CONFIG } from '../types';
+import type { SVELTEKIT_DATA_ENPOINT_CONFIG } from '../types/sveltekit-endpoits.type';
+import type { SVELTEKIT_STARTER_API_CONFIG } from '../types/sveltekit-starter-api-config.type';
 import type { IEnvironmentConfig } from './ienvironment.interface';
 import type { ISveltekitDBConfig } from './isveltekit-db-config.interface';
 import type { ITwitterConfig } from './itwitter-config.interface';
