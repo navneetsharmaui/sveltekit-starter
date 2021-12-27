1c506783b4bb2cfb3d767dd183511e01403bbb36
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-27 20:39:36 +0530

    Updated the project route view

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,25 +1,9 @@
-<script lang="ts" context="module">
-	import { browser, dev } from '$app/env';
-
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
-
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	// Start: Local Imports
 	// Components
 	import HeadTags from '$components/head-tags/HeadTags.svelte';
-	import ExternalLink from '$ui/components/external-link/ExternalLink.svelte';
 	import ProjectCard from '$components/project-card/ProjectCard.svelte';
+	import ExternalLink from '$ui/components/external-link/ExternalLink.svelte';
 
 	// Models
 	import type { IMetaTagProperties } from '$models/interfaces/imeta-tag-properties.interface';
