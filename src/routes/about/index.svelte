af0c61c738fc4012e45aa0b832388cc474a73924
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-27 20:38:33 +0530

    Updated the about route view

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,19 +1,3 @@
-<script lang="ts" context="module">
-	import { browser, dev } from '$app/env';
-
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
-
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	// Start: Local Imports
 	// Components
