5521e5a30f48128302d95a2abca1a080bf90364f
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-01-02 21:39:12 +0530

    Updated the user class model with stricter type properties

--- src/lib/models/classes/user.model.ts
+++ src/lib/models/classes/user.model.ts
@@ -2,9 +2,9 @@ import type { IUser } from '$models/interfaces/iuser.interface';
 import type { IDeserializable } from '$models/interfaces/ideserializable.interface';
 
 export class UserModel implements IDeserializable<IUser>, IUser {
-	public name: string;
-	public job: string;
-	public id: string;
+	public name!: string;
+	public job!: string;
+	public id!: string;
 
 	deserialize(input: IUser): this {
 		Object.assign(this, input);
