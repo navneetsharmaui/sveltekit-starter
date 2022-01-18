c40bccf4785dabec47d49a221c3d2354d10901fb
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-01-18 12:29:50 +0530

    Removed the navigation links from the header navigation bar

--- src/lib/shared/ui/components/navigation-bar/NavigationBar.svelte
+++ src/lib/shared/ui/components/navigation-bar/NavigationBar.svelte
@@ -3,16 +3,8 @@
 	import { createEventDispatcher } from 'svelte';
 
 	// Models
-	import type { IHeaderNavLink } from '$models/interfaces/iheader-nav-link.interface';
 
 	// Exports
-	/**
-	 * @type {IHeaderNavLink}
-	 */
-	export let navLinks!: IHeaderNavLink[];
-	export let logoImage = '';
-	export let title = '';
-	export let useTitleAndLogo = false;
 	export let useThemeModeButton = true;
 
 	// Local Properties
@@ -34,9 +26,7 @@
 		on:click="{() => toggleTheme()}"
 		aria-label="Toggle Dark Mode"
 		type="button"
-		class="{useTitleAndLogo
-			? 'sticky-theme-mode-button w-10 h-10 p-3 bg-gray-200 rounded-full dark:bg-gray-800'
-			: 'w-10 h-10 p-3 bg-gray-200 rounded-full dark:bg-gray-800'}"
+		class="{'sticky-theme-mode-button w-10 h-10 p-3 bg-gray-200 rounded-full dark:bg-gray-800'}"
 	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -63,38 +53,3 @@
 		</svg>
 	</button>
 {/if}
-<nav
-	class="flex flex-wrap items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100"
->
-	<!-- <a href="#skip" class="skip-nav"> Skip to content </a> -->
-	{#if useTitleAndLogo}
-		<div class="w-auto p-1 text-gray-900 dark:text-gray-100 font-bold">
-			<a
-				sveltekit:prefetch
-				href="/"
-				class="flex flex-row h-12 justify-center items-center"
-				aria-label="{title}"
-			>
-				<img
-					src="{logoImage}"
-					alt="{title}"
-					width="3rem"
-					height="3rem"
-					class="w-12 h-12 mr-2 rounded-full"
-				/>
-				{title}
-			</a>
-		</div>
-	{/if}
-	<div class="flex flex-row items-center">
-		{#each navLinks as navLink, index (navLink.path)}
-			<a
-				sveltekit:prefetch
-				href="{navLink.path}"
-				class="p-1 text-gray-900 sm:p-4 dark:text-gray-100"
-			>
-				{navLink.label}
-			</a>
-		{/each}
-	</div>
-</nav>
