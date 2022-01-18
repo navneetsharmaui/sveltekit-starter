16b70e6a456100aa0f7b7f6bfc45b10b75b70a1f
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-01-18 12:30:18 +0530

    Removed the navigation links from the header navigation bar

--- src/lib/shared/ui/components/header/Header.svelte
+++ src/lib/shared/ui/components/header/Header.svelte
@@ -2,7 +2,6 @@
 	// Start: Local Imports
 
 	// Models
-	import type { IHeaderNavLink } from '$models/interfaces/iheader-nav-link.interface';
 
 	// Components
 	import { NavigationProgressBar } from '$ui/components/navigation-progress-bar';
@@ -10,18 +9,8 @@
 	// End: Local Imports
 
 	// Start: Exported Properties
-	/**
-	 * @type {string}
-	 */
-	export let logoImage = '';
-	export let title = '';
-	export let useTitleAndLogo!: boolean;
 	export let useThemeModeButton!: boolean;
 
-	/**
-	 * @type {IHeaderNavLink}
-	 */
-	export let navLinks!: IHeaderNavLink[];
 	// End: Exported Properties
 
 	// Local properties
@@ -33,11 +22,7 @@
 
 <!-- Start: Navigation bar -->
 <NavigationBar
-	navLinks="{navLinks}"
-	logoImage="{logoImage}"
-	title="{title}"
 	useThemeModeButton="{useThemeModeButton}"
-	useTitleAndLogo="{useTitleAndLogo}"
 	on:toggleTheme
 />
 <!-- End: Navigation bar -->
