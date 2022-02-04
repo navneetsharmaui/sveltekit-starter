d4d3c799170f5077ba1d85bee958179c2bd43a5c
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-02-04 17:09:11 +0530

    Updated the dashboard routes

--- src/routes/dashboard/__layout.reset.svelte
+++ src/routes/dashboard/__layout.reset.svelte
@@ -1,13 +1,3 @@
-<script lang="ts" context="module">
-	import type { Load } from '@sveltejs/kit';
-
-	export const load: Load = ({ page }) => ({
-		props: {
-			path: page.path,
-		},
-	});
-</script>
-
 <script lang="ts">
 	// Start: Local Imports
 	import '../../styles/tailwind.css';
@@ -19,11 +9,12 @@
 	// Core services
 
 	// Components
-	import RouteTransition from '$ui/components/route-transition/RouteTransition.svelte';
+	import DashboardFooter from '$ui/components/footers/DashboardFooter.svelte';
+
+	import { page } from '$app/stores';
 	// End: Local Imports
 
 	// Start: Local component properties
-	export let path = '';
 
 	// End: Local component properties
 
@@ -37,38 +28,81 @@
 		<aside
 			class="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-col md:flex-nowrap md:overflow-hidden shadow-md flex flex-row flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6 bg-white border-r-[1px] border-solid border-gray-200"
 		>
-			<div class="px-4 min-w-full mt-4 mb-8">
-				<h1 class="font-bold text-xl text-green-500">Sveltekit Starter</h1>
+			<div class="px-4 min-w-full my-4">
+				<a sveltekit:prefetch href="/">
+					<h1 class="font-bold text-xl text-green-500">Sveltekit Starter</h1>
+				</a>
+			</div>
+			<div class="w-full my-2">
+				<hr />
 			</div>
 			<nav class="px-4 w-full">
 				<ul>
-					<li class="font-semibold cursor-pointer py-2"> Home </li>
-					<li class="font-semibold cursor-pointer py-2"> Dashboard </li>
-					<li class="font-semibold cursor-pointer py-2"> Project </li>
-					<li class="font-semibold cursor-pointer py-2"> Users </li>
+					<li
+						class="font-semibold cursor-pointer py-2 {$page.path === '/dashboard'
+							? 'text-red-500'
+							: ''}"
+					>
+						<a sveltekit:prefetch href="/dashboard"> Dashboard </a>
+					</li>
+					<li
+						class="font-semibold cursor-pointer py-2 {$page.path.includes(
+							'/dashboard/accounts',
+						)
+							? 'text-red-500'
+							: ''}"
+					>
+						<a sveltekit:prefetch href="/dashboard/accounts"> Accounts </a>
+					</li>
+					<li
+						class="font-semibold cursor-pointer py-2 {$page.path.includes(
+							'/dashboard/sales',
+						)
+							? 'text-red-500'
+							: ''}"
+					>
+						<a sveltekit:prefetch href="/dashboard/sales"> Sales </a>
+					</li>
+					<li
+						class="font-semibold cursor-pointer py-2 {$page.path.includes(
+							'/dashboard/expenses',
+						)
+							? 'text-red-500'
+							: ''}"
+					>
+						<a sveltekit:prefetch href="/dashboard/expenses"> Expenses </a>
+					</li>
+					<li
+						class="font-semibold cursor-pointer py-2 {$page.path.includes(
+							'/dashboard/reports',
+						)
+							? 'text-red-500'
+							: ''}"
+					>
+						<a sveltekit:prefetch href="/dashboard/reports"> Reports </a>
+					</li>
 				</ul>
 			</nav>
 		</aside>
 		<article
-			class="relative md:ml-64 flex flex-col w-[calc(100vw - 15rem - 1rem)] p-0 h-auto m-0 overflow-y-auto"
+			class="relative md:ml-64 flex flex-col w-full md:w-[calc(100%-15rem-1rem)] p-0 m-0 min-h-full h-full overflow-y-auto"
 		>
 			<header
 				class="p-3 w-full flex flex-row justify-between items-center shadow-md h-14 border-b-[1px] border-r-[1px] border-solid border-gray-200"
 			>
-				<h1> Dashboard </h1>
+				<h1 class="font-semibold"> Dashboard </h1>
 				<div
 					class="block border-2 border-solid border-gray-100 rounded-full bg-gray-50 h-11 w-11"
 				>
 				</div>
 			</header>
-			<section class="px-4">
+			<section class="block w-full min-h-[calc(100vh-56px-77px)]">
 				<!-- Start: Defaull layout slot -->
-				<RouteTransition referesh="{path}">
-					<slot />
-				</RouteTransition>
+				<slot />
 			</section>
 			<!-- End: Defaull layout slot -->
 			<!-- Start: Footer -->
+			<DashboardFooter />
 			<!-- End: Footer -->
 		</article>
 	</main>
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -3,6 +3,8 @@
 
 	// Components
 	import HeadTags from '$components/head-tags/HeadTags.svelte';
+	import PageVisitsTable from '$components/tables/PageVisitsTable.svelte';
+	import StatsCard from '$ui/components/cards/StatsCard.svelte';
 
 	// Models
 	import type { IMetaTagProperties } from '$models/interfaces/imeta-tag-properties.interface';
@@ -10,9 +12,6 @@
 	// Exports
 
 	// Start: Local component properties
-	/**
-	 * @type {IMetaTagProperties}
-	 */
 	const metaData: Partial<IMetaTagProperties> = {
 		title: 'Dashboard | Sveltekit Starter',
 		description: 'Dashboard page of Sveltekit starter project',
@@ -26,20 +25,72 @@
 <HeadTags metaData="{metaData}" />
 <!-- End: Header Tag -->
 
-<div class="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-	<h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-		Dashboard
-	</h1>
-	<div class="mb-8">
-		<p class="text-gray-600 dark:text-gray-400 mb-4">
-			This the dashboard view for my portfolio to track the metric related to the various
-			platforms I use and are availabe for the public use. Right now it just tracks GitHub
-			metrics.
-		</p>
-		<p class="text-gray-600 dark:text-gray-400 mb-4">
-			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque obcaecati est dolor
-			voluptatum tenetur repellendus esse consequatur, earum, voluptatem animi porro
-			distinctio itaque. Ratione dolore distinctio corporis, vitae totam est.
-		</p>
+<div class="relative pt-10">
+	<div class="px-4 md:px-10 mx-auto w-full">
+		<div>
+			<!-- Card stats -->
+			<div class="flex flex-col flex-nowrap lg:flex-row lg:flex-wrap">
+				<div class="w-full lg:w-[calc(50%-0.25rem)] m-0 lg:ml-1 lg:mb-1">
+					<StatsCard
+						statSubtitle="ACCOUNTS"
+						statTitle="350,897"
+						statArrow="up"
+						statPercent="3.48"
+						statPercentColor="text-emerald-500"
+						statDescripiron="Since last month"
+						statIconName="far fa-chart-bar"
+						statIconColor="bg-red-500"
+					/>
+				</div>
+				<div class="w-full lg:w-[calc(50%-0.25rem)] m-0 lg:ml-1 lg:mb-1">
+					<StatsCard
+						statSubtitle="SALES"
+						statTitle="2,356"
+						statArrow="down"
+						statPercent="3.48"
+						statPercentColor="text-red-500"
+						statDescripiron="Since last week"
+						statIconName="fas fa-chart-pie"
+						statIconColor="bg-red-500"
+					/>
+				</div>
+				<div class="w-full lg:w-[calc(50%-0.25rem)] m-0 lg:ml-1 lg:mb-1">
+					<StatsCard
+						statSubtitle="EXPENSES"
+						statTitle="924"
+						statArrow="down"
+						statPercent="1.10"
+						statPercentColor="text-orange-500"
+						statDescripiron="Since yesterday"
+						statIconName="fas fa-users"
+						statIconColor="bg-pink-500"
+					/>
+				</div>
+				<div class="w-full lg:w-[calc(50%-0.25rem)] m-0 lg:ml-1 lg:mb-1">
+					<StatsCard
+						statSubtitle="PERFORMANCE"
+						statTitle="49,65%"
+						statArrow="up"
+						statPercent="12"
+						statPercentColor="text-emerald-500"
+						statDescripiron="Since last month"
+						statIconName="fas fa-percent"
+						statIconColor="bg-red-500"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="w-full py-10 px-4">
+	<hr />
+</div>
+<div class="block px-4 md:px-10 mx-auto w-full h-auto">
+	<div>
+		<div class="flex flex-wrap">
+			<div class="w-full mb-12 xl:mb-0">
+				<PageVisitsTable />
+			</div>
+		</div>
 	</div>
 </div>
