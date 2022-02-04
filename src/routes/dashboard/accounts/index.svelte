35947312bb8807aeec4f1ccdade29549ac9aa9cc
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-02-04 17:10:20 +0530

    Updated the account nested routes to the dashboard routes

---
+++ src/routes/dashboard/accounts/index.svelte
@@ -0,0 +1,9 @@
+<div class="w-full flex flex-col px-4 md:px-10 mx-auto">
+	<div class="w-full py-4">
+		<h1 class="font-semibold text-xl"> Accounts </h1>
+	</div>
+	<div class="w-full py-4">
+		<hr />
+	</div>
+	<div class="w-full py-4"> </div>
+</div>
